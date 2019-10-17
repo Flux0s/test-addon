6bb69eb8b4e1ba56f5b903d758d1ecef6409232c
Jason <Flux0s@users.noreply.github.com>
2019-10-16 23:36:43 -0400

    added main

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,8 @@ Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
   return functionexample::Init(env, exports);
 }
 
+Napi::Object InitChess(Napi::Env env, Napi::Object exports) {
+	return 
+}
+
 NODE_API_MODULE(testaddon, InitAll)
